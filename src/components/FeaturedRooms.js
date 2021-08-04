1c13fb2a49c7e57e0fb37baef18f9cf0cdc6066e
Gabi <gabee_88@yahoo.com>
2021-08-04 09:35:11 +0200

    feat: created loading and room component

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { RoomContext } from "../Context";
+import Loading from "./Loading";
+import Room from "../components/Room";
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
@@ -7,6 +9,12 @@ export default class FeaturedRooms extends Component {
     const { featuredRooms: rooms } = this.context;
     console.log(rooms);
 
-    return <div>hello from featured rooms</div>;
+    return (
+      <div>
+        hello from featured rooms
+        <Room />
+        <Loading />
+      </div>
+    );
   }
 }
